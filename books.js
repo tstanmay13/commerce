23b4ca41375e8bf96077cb0efc8033e0cb93fefb
tstanmay13 <tstanmay13@gmail.com>
2022-07-12 19:28:42 -0500

    books page

--- books.js
+++ books.js
@@ -1,4 +1,79 @@
+function renderBooks(filter) {
+  const booksWrapper = document.querySelector(".books");
+  const books = getBooks();
+  // console.log(books);
 
+  if (filter == "LOW_TO_HIGH") {
+    const filteredBooks = books.sort(
+      (a, b) => a.originalPrice - b.originalPrice
+    );
+  } else if (filter == "HIGH_TO_LOW") {
+    const filteredBooks = books.sort(
+      (a, b) => b.originalPrice - a.originalPrice
+    );
+  } else if (filter == "RATING") {
+    const filteredBooks = books.sort((a, b) => b.rating - a.rating);
+  }
+
+  console.log(ratingHTML);
+
+  const booksHTML = books
+    .map((book) => {
+      return `<div class="book">
+    <figure class="book__img--wrapper">
+      <img class="book__img" src='${book.url}' alt="">
+    </figure>
+    <div class="book__title">
+      ${book.title}
+    </div>
+    <div class="book__ratings">
+      ${ratingHTML(book.rating)}
+    </div>
+    <div class="book__price">
+      ${priceHTML(book.originalPrice, book.salePrice)}
+    </div>
+  </div>`;
+    })
+    .join("");
+
+  booksWrapper.innerHTML = booksHTML;
+  // console.log(booksHTML);
+}
+
+function priceHTML(originalPrice, salePrice){
+  if(!salePrice){
+    return `$` + originalPrice.toFixed(2);
+  }
+  else {
+    return `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span> $${salePrice.toFixed(2)}`
+  }
+
+}
+
+
+
+function ratingHTML(rating) {
+  let ratingHTML = "";
+
+  for (let i = 0; i < Math.floor(rating); i++) {
+    ratingHTML += `<i class="fas fa-star"></i>\n`;
+  }
+
+  if (!Number.isInteger(rating)) {
+    ratingHTML += `<i class="fas fa-star-half-alt"></i>\n`;
+  }
+
+  return ratingHTML;
+}
+
+function filterBooks(event) {
+  // console.log(event.target.value)
+  renderBooks(event.target.value);
+}
+
+setTimeout(() => {
+  renderBooks();
+});
 
 // FAKE DATA
 function getBooks() {
@@ -6,7 +81,7 @@ function getBooks() {
     {
       id: 1,
       title: "Crack the Coding Interview",
-                url: "assets/crack the coding interview.png",
+      url: "assets/crack the coding interview.png",
       originalPrice: 49.95,
       salePrice: 14.95,
       rating: 4.5,
